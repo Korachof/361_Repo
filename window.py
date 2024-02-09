f84114b8a22bf67484a12879c0c3a3c97de8bdf7
Christopher Partin <114110894+Korachof@users.noreply.github.com>
2024-02-09 12:20:07 -0800

    Update window.py
    Create the window ui class, that contains an init function that will initalize the main start page window and start at the home page frame. Also created a create_frame class that destroys the previous frame and places another one where the old one was. This will be used to cycle through the different pages as the user clicks buttons and moves about the program.

--- window.py
+++ window.py
@@ -1,1 +1,32 @@
+import tkinter as tk
+from tkinter import *
+import sys
 
+
+class WindowScreen(tk.Tk):
+  """Class to create the window and control the frames"""
+    def __init__(self):
+        super().__init__()
+        
+        self.title("MTG Staple Compiler")
+        self.geometry("1280x720")
+        
+        Grid.rowconfigure(self,0,weight=1)
+        Grid.columnconfigure(self,0,weight=1)
+        self.frame_hash = {0: StartPage, 1: ColorSelectPage, 2: ResultsPage}
+        self.current_window = 0
+        self.button_command = self.frame_hash[self.current_window]
+        self.frame = self.create_frame(self.current_window)
+        
+
+    def create_frame(self, buttonNum):
+      """A function to control the different frames/pages on the window"""
+        for widgets in self.button_command.winfo_children(self):
+            widgets.destroy()
+        self.current_window = buttonNum
+        self.button_command = self.frame_hash[buttonNum]
+        self.button_command.tkraise(self)
+        
+        self.frame = self.button_command(self.frame_hash, self)
+        
+        self.frame.grid(row=0, column=0)
