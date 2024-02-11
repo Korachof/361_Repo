aa237508f9aeab9065bed1f01e2ef8b44ba0af72
Christopher Partin <114110894+Korachof@users.noreply.github.com>
2024-02-11 10:42:11 -0800

    Update deck_build_algo.py
    Used Frank Karsten's formula on Channelfireball to make a simple function to figure out proper land count for edh decks. Made a function for it.

--- deck_build_algo.py
+++ deck_build_algo.py
@@ -1,1 +1,8 @@
+# algorithm for quantity of lands provided by Frank Karsten 
+# https://www.channelfireball.com/article/How-Many-Lands-Do-You-Need-in-Your-Deck-An-Updated-Analysis/cd1c1a24-d439-4a8e-b369-b936edb0b38a/
+
+
+def land_count(avg_mana_cost, num_cheap_draw_ramp):
+  num_lands = 31.42 + 3.13 * avg_mana_cost – 0.28 * num_cheap_draw_ramp
+  return num_lands
 
