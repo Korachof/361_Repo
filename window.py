8f9f59a65466d593613fb6e039f5656684657046
Christopher Partin <114110894+Korachof@users.noreply.github.com>
2024-02-11 10:33:54 -0800

    Update window.py
    Filled out more of the start page. Created labels for text for simple instructions, as well as buttons for the various options. When the buttons are clicked, they will switch the frames using the frame hashmap after destroying the current frame. 
    
    Need to flesh it out a bit by adding another option or two and create at least one other page before testing thoroughly.

--- window.py
+++ window.py
@@ -38,3 +38,73 @@ class StartPage(tk.Frame):
         self.frameHash = frameHash
         self.mainWindow = mainWindow
         self.label = tk.Label(self, text="Main Page")
+        
+        # Configure the row and 5 columns for the buttons. 
+        Grid.rowconfigure(self,0,weight=1)
+        Grid.rowconfigure(self,1,weight=1)
+        Grid.rowconfigure(self,2,weight=1)
+        Grid.rowconfigure(self,3,weight=1)
+        Grid.rowconfigure(self,4,weight=1)
+        Grid.rowconfigure(self,5,weight=1)
+        Grid.columnconfigure(self,0,weight=1)
+        Grid.columnconfigure(self,1,weight=1)
+        Grid.columnconfigure(self,2,weight=1)
+        Grid.columnconfigure(self,3,weight=1)
+        Grid.columnconfigure(self,4,weight=1)
+
+      
+        # create the button images 
+        self.start_button = PhotoImage(file="button_images/start_button.png")   
+
+        # create text labels 
+        self.welcome_label = tk.Label(self, text="Welcome to the MTG Staple Compiler")
+        self.choose_label = tk.Label(self, text="Please choose from the options below")
+
+        # set the start button with the start button image
+        self.start = tk.Button(self,
+                    image=self.start_button)
+
+        # set the window frame to the start page frame
+        self.options_frame = tk.Frame(self)
+
+        # set the frame on the grid
+        self.options_frame.grid(row=3, column=2, padx=1, pady=1)
+
+        # Create buttons with the different options for the user
+        self.color_choice = tk.Button(self.options_frame, 
+                                      text="Find Deck Staples", 
+                                      bg="#2f9fd6",
+                                      fg="white", 
+                                      activebackground="#146d99", 
+                                      activeforeground="white", 
+                                      font="Garamond",
+                                      command=lambda: self.mainWindow.create_frame(1))
+        
+        self.lands_algorithm = tk.Button(self.options_frame, 
+                                     text="Calculate How Many Lands Your Deck Needs", 
+                                     bg="#2f9fd6", 
+                                     fg="white", 
+                                     activebackground="#146d99", 
+                                     activeforeground="white", 
+                                     font="Garamond",
+                                     command=lambda: self.mainWindow.create_frame(2))
+        
+        self.exit_button = tk.Button(self.options_frame,
+                                     text="Exit",
+                                     bg="#2f9fd6",
+                                     fg="white", 
+                                     activebackground="#146d99", 
+                                     activeforeground="white",
+                                     font="Garamond", 
+                                     command=sys.exit)
+
+        #  set the buttons and labels on the grid 
+        self.welcome_label.grid(row=0, column=2, padx=1, pady=1)
+        self.choose_label.grid(row=1, column=2, padx=1, pady=1)
+        self.start.grid(row=2, column=2, padx=1, pady=1)
+        self.color_choice.grid(row=0, column=0, padx=1, pady=2, sticky="WES")
+        self.lands_algorithm.grid(row=1, column=0, padx=1, pady=2, sticky="WEN")
+        self.exit_button.grid(row=2, column=0, padx=1, pady=2, sticky="WEN")
+        
+
+
